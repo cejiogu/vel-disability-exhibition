6169d8d8e058d08ca6ba513d1971557b095fd017
Chimdi Ejiogu <chimdije@gmail.com>
2026-04-02 14:29:07 -0400

    Prepare for deployment

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,7 @@
 import os
 from pathlib import Path
-import dotenv
-
 
 class Config:
-    dotenv.load_dotenv()
     BASE_DIR = Path(__file__).resolve().parent.parent
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
